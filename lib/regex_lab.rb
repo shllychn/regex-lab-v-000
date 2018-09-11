b8400d5d2fa7d5b5316d3473b326ef3babdc93fe
Shelly Chen <shellyjychen@gmail.com>
2018-09-11 05:27:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,13 @@
 def starts_with_a_vowel?(word)
   if word.match(/^[aeiouAEIOU]/)
     true
-  else false
+  else
+    false
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.match(/^un+\w+ing)
 end
 
 def words_five_letters_long(text)
