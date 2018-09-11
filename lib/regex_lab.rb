cd259cbc49cb7a9706af2a5870f66df65eaa7efb
Shelly Chen <shellyjychen@gmail.com>
2018-09-11 05:54:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\b[A-Z][,.?!]/)
+  if text.scan(/\b[A-Z][,.?!]/)
+    true
+  else
+    false
 end
 
 def valid_phone_number?(phone)
